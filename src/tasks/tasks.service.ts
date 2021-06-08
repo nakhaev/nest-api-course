e7ce3f36d016108ed219e8319e8c3b7b370d53a0
Alexey  Nakhaev <anakhaev@dfusiontech.com>
2021-06-08 07:33:53 +0300

    prettier settings was changed the code was reformatted getAllTasks method and endpoint were added

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+    private tasks = [
+        {
+            name: 'foo',
+            status: 'open',
+        },
+    ];
+
+    getAllTasks() {
+        return this.tasks;
+    }
+}
